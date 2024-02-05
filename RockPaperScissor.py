44b2eea210e1d0a6acfbaf6bd1def24fe887c1aa
TEJAS-SAI-PRASHAD-K <135939991+TEJAS-SAI-PRASHAD-K@users.noreply.github.com>
2024-02-05 21:23:46 +0530

    second_commit

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,60 +1,56 @@
 def winning(m,n):
 
     # numbering {1:"ROCK",2:"PAPER",3:"SCISSORS"}
-
-    while (m==1 and n==2) and (m==2 and n==1):
         if m==1 and n==2:
             print("USER : ROCK VS COMPUTER : PAPER")
-            print("COMPUTER WINS USING PAPER")
-            # return comp+=1
+            print("COMPUTER WINS USING PAPER \n")
+            return "comp"
         elif m==2 and n==1:
             print("USER : PAPER VS COMPUTER : ROCK")
-            print("USER WINS USING PAPER")
-            # return user+=1
-    while (m==1 and n==3) and (m==3 and n==1):
-        if m==1 and n==3:
+            print("USER WINS USING PAPER \n")
+            return "user"
+        elif m==1 and n==3:
             print("USER : ROCK VS COMPUTER : SCISSORS")
-            print(" USER WINS USING ROCK")
-            # return user+=1
+            print(" USER WINS USING ROCK \n")
+            return "user"
         elif m==3 and n==1:
             print("USER : SCISSORS VS COMPUTER : ROCK")
-            print("COMPUTER WINS USING ROCK")
-            # return comp+=1
-    while (m==2 and n==3) and (m==3 and n==2):
-        if m==2 and n==3:
+            print("COMPUTER WINS USING ROCK \n")
+            return "comp"
+        elif m==2 and n==3:
             print("USER : PAPER VS COMPUTER : SCISSORS")
-            print("COMPUTER WINS USING SCISSORS")
-            # return comp+=1
+            print("COMPUTER WINS USING SCISSORS \n")
+            return "comp"
         elif m==3 and n==2:
             print("USER : SCISSORS VS COMPUTER : PAPER")
-            print("USER WINS USING SCISSORS")
-            # return user+=1
+            print("USER WINS USING SCISSORS \n")
+            return "user"
             
 
 import random
 
-brk=False
+brk=True
+comp=0
+user=0
 
-y=3
-for i in range(y):
-    if brk:
-        break
-    else:
-        x=int(input("Enter the choice:"))
+while brk:    
         print("1 -- ROCK")
         print("2 -- PAPER")
         print("3 -- SCISSORS")
-
+        x=int(input("Enter the choice:"))
+        print("\n")
+        
         rd=random.randint(1,3)
 
-        comp=1
-        user=2
-
         if x==rd:
-            print("DRAW")
-            break
+            print("-----DRAW----- \n")
+            print("-----PLAY AGAIN----- \n")
         else:
-            x=winning(x,rd)
+            y=winning(x,rd)
+            if y=="comp":
+                 comp+=1
+            else:
+                 user+=1
             if user==2 or comp==2:
                 brk=True
                 break
